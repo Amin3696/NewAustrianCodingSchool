615acdae66fabcaa59d0f7ac88eabc7c6fe8182b
amin3696 <a11shahimian@gmail.com>
2019-01-22 00:08:29 +0100

    2.Pokemon

--- src/exercises/Week10MoreStream/exercise01Pokemon/Pokemonexplorer.java
+++ src/exercises/Week10MoreStream/exercise01Pokemon/Pokemonexplorer.java
@@ -1,5 +1,6 @@
 package exercises.Week10MoreStream.exercise01Pokemon;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -8,36 +9,61 @@ public class Pokemonexplorer {
     private PokemonReader pokemonReader = new PokemonReader();
     private List<Pokemon> pokemons = pokemonReader.readPokemons();
 
-    public List<Pokemon> allPokemones() {
+    public List<Pokemon> getListOfPokemones() {
         return pokemons;
     }
 
-    public Integer amountOfPokemons() {
+    public Integer getNumberOfPokemons() {
         return pokemons.size();
     }
 
-    public Long numberOfNoTypeII() {
+
+    public Long getNumberOfPokemonsWithoutTypeII() {
         return pokemons.stream()
                 .filter(e -> e.getType2().equalsIgnoreCase(""))
                 .count();
     }
 
-    public List<Pokemon> fastestPokemons() {
+    public List<Pokemon> getFastestPokemons() {
         return pokemons.stream()
                 .filter(e -> e.getSpeed() == 100)
                 .collect(Collectors.toList());
     }
 
 
-    public Map<String, List<Pokemon>> pokemonsBelongToTypeI() {
+    public Map<String, List<Pokemon>> getTypeI() {
         return pokemons.stream()
                 .collect(Collectors.groupingBy(pokemon -> pokemon.getType1(), Collectors.toList()));
     }
 
-    public Map<String, List<Pokemon>> pokemonsBelongToTypeII() {
+    public Map<String, List<Pokemon>> getTypeII() {
         return pokemons.stream()
                 .collect(Collectors.groupingBy(pokemon -> pokemon.getType2(), Collectors.toList()));
     }
 
+    public Map<Integer, List<Pokemon>> findFirstFiveTotal() {
+        return pokemons.stream()
+                .sorted(Comparator.comparing(Pokemon::getTotal).reversed())
+                .limit(5)
+                .collect(Collectors.groupingBy(e -> e.getTotal(), Collectors.toList()));
+    }
 
+    public List<Pokemon> find5LegendaryPokemons() {
+        return pokemons.stream()
+                .filter(e -> Boolean.valueOf(e.getLegendary()) == true)
+                .limit(5)
+                .collect(Collectors.toList());
+    }
+
+    public Long getNumberOfLegendaryPokemonsWithSpeedFrom80() {
+        return pokemons.stream()
+                .filter(e -> Boolean.valueOf(e.getLegendary()) == true)
+                .filter(e->e.getSpeed()>80)
+                .count();
+    }
+
+    public Map<Boolean, Long> getNonLegendary() {
+        return pokemons.stream()
+                .collect(Collectors.groupingBy(e -> e.getLegendary(), Collectors.counting()));
+    }
 }
--- src/exercises/Week10MoreStream/exercise01Pokemon/PokemonsApp.java
+++ src/exercises/Week10MoreStream/exercise01Pokemon/PokemonsApp.java
@@ -11,24 +11,24 @@ public class PokemonsApp {
         Pokemonexplorer pokemonexplorer = new Pokemonexplorer();
 
         System.out.println("---------list of Pokemons:-------");
-        System.out.println(pokemonexplorer.allPokemones());
+        System.out.println(pokemonexplorer.getListOfPokemones());
         System.out.println();
 
-        System.out.println("1-how many pokemons are exict in this file?");
-        System.out.println(pokemonexplorer.amountOfPokemons() + " Pokemons are in this List");
+        System.out.println("1-how many pokemons are exist in this file?");
+        System.out.println(pokemonexplorer.getNumberOfPokemons() + " Pokemons are in this List");
         System.out.println();
 
         System.out.println("2-How many Pokemons don't have a Type2?");
-        System.out.println(pokemonexplorer.numberOfNoTypeII() + " don't have typeII");
+        System.out.println(pokemonexplorer.getNumberOfPokemonsWithoutTypeII() + " don't have typeII");
         System.out.println();
 
         System.out.println("3-Pokemons with Maximum Speed: ");
-        System.out.println(pokemonexplorer.fastestPokemons());
-        System.out.println(pokemonexplorer.fastestPokemons().size()+" pokemons have the maximum speed.");
+        System.out.println(pokemonexplorer.getFastestPokemons());
+        System.out.println(pokemonexplorer.getFastestPokemons().size()+" pokemons have the maximum speed.");
         System.out.println();
 
-        System.out.println("4-6-display the list of TypeI");
-        Map<String, List<Pokemon>> typeI = pokemonexplorer.pokemonsBelongToTypeI();
+        System.out.println("4-display the list of TypeI");
+        Map<String, List<Pokemon>> typeI = pokemonexplorer.getTypeI();
         System.out.println(typeI.keySet());
         System.out.println();
 
@@ -37,13 +37,29 @@ public class PokemonsApp {
         System.out.println();
 
         System.out.println("6-display the list of TypeII");
-        Map<String, List<Pokemon>> typeII = pokemonexplorer.pokemonsBelongToTypeII();
+        Map<String, List<Pokemon>> typeII = pokemonexplorer.getTypeII();
         List<String> typesII = typeII.keySet().stream().sorted().collect(Collectors.toList());
         System.out.println("TypeII "+typesII);
         System.out.println();
 
         System.out.println("7-Pokemons related to typeII:");
         System.out.println(typeII);
+        System.out.println();
+
+        System.out.println("8-List the first 5 Pokemons with maximum of Total");
+        System.out.println(pokemonexplorer.findFirstFiveTotal());
+        System.out.println();
+
+        System.out.println("9-How many Pokemons are Legendary with speed of at least 80? ");
+        System.out.println(pokemonexplorer.getNumberOfLegendaryPokemonsWithSpeedFrom80());
+        System.out.println();
+
+        System.out.println("10-List of 5 Legendary Pokemons:");
+        System.out.println(pokemonexplorer.find5LegendaryPokemons());
+        System.out.println();
+
+        System.out.println("11-Number of Non-Legendary and Legendary Pokemons:");
+        System.out.println(pokemonexplorer.getNonLegendary());
 
     }
 }

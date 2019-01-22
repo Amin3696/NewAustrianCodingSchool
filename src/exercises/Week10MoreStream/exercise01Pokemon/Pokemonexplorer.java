0d7886db2a78d1a752258cd5d5953a756e00ff14
amin3696 <a11shahimian@gmail.com>
2019-01-22 15:07:25 +0100

    askNew11-17

--- src/exercises/Week10MoreStream/exercise01Pokemon/Pokemonexplorer.java
+++ src/exercises/Week10MoreStream/exercise01Pokemon/Pokemonexplorer.java
@@ -1,8 +1,6 @@
 package exercises.Week10MoreStream.exercise01Pokemon;
 
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Pokemonexplorer {
@@ -58,12 +56,47 @@ public class Pokemonexplorer {
     public Long getNumberOfLegendaryPokemonsWithSpeedFrom80() {
         return pokemons.stream()
                 .filter(e -> Boolean.valueOf(e.getLegendary()) == true)
-                .filter(e->e.getSpeed()>80)
+                .filter(e -> e.getSpeed() > 80)
                 .count();
+
     }
 
     public Map<Boolean, Long> getNonLegendary() {
         return pokemons.stream()
                 .collect(Collectors.groupingBy(e -> e.getLegendary(), Collectors.counting()));
+    }
+
+    public Set<Pokemon> showTheDefensiveLegendarys() {
+        return pokemons.stream()
+                .filter(e -> Boolean.valueOf(e.getLegendary()) == true)
+                .filter(e -> Integer.valueOf(e.getDefense()) > 100)
+                .limit(5)
+                .collect(Collectors.toSet());
+
+    }
+
+
+    public Optional<Pokemon> showTheMostPowerfulInAttack() {
+        return pokemons.stream()
+                .sorted(Comparator.comparing(Pokemon::getAttack).reversed())
+                .findFirst();
+    }
+
+    public Optional<Integer> getTheTotalAmountOfHp() {
+        return pokemons.stream()
+                .map(e -> Integer.valueOf(e.getHp()))
+                .limit(10)
+                .reduce((e1, e2) -> e1 + e2);
+    }
+
+    public boolean isNotAnyPokemonWithTotalMoreThan800() {
+        return pokemons.stream()
+                .noneMatch(e -> e.getTotal() > 800);
+
+
+    }
+    public boolean isAnyPokemonWithTotalMoreThanValue() {
+        return pokemons.stream()
+                .anyMatch(e -> e.getTotal() > 700);
     }
 }

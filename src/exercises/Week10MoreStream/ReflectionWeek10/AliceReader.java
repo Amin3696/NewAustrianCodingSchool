9c4caa9549c5567d9e44b3f63507174c7c6eb0ce
amin3696 <a11shahimian@gmail.com>
2019-01-25 11:43:02 +0100

    addLongestWord

--- src/exercises/Week10MoreStream/ReflectionWeek10/AliceReader.java
+++ src/exercises/Week10MoreStream/ReflectionWeek10/AliceReader.java
@@ -4,25 +4,33 @@ import exercises.week4DateStructure.ex09File.FileReader;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AliceReader {
 
-    private FileReader fileReader = new FileReader();
 
-    public List<String[]> readAlice() {
-        return  fileReader.asStream("exercises/Week10MoreStream/ReflectionWeek10/alice.txt")
-
-                .map(e -> e.toLowerCase())
-                .map(line -> line.replaceAll(",", " "))
-                .map(line -> line.replaceAll(";", " "))
-                .map(line -> line.replaceAll("'s", " "))
-                .map(line -> line.replaceAll("'", " "))
-                .map(line -> line.replaceAll("‘", " "))
-                .map(line -> line.replaceAll(":", " "))
-                .map(line -> line.replaceAll("#", " "))
-                .map(line -> line.replaceAll("/", " "))
-                .map(line -> line.replaceAll("-", " "))
+    public List<String> readAliceWords(String filePath) {
+        return  new FileReader().asStream(filePath)
+                .map(String::toLowerCase)
+                .map(e -> e.replaceAll(",", " "))
+                .map(e -> e.replaceAll(";", " "))
+                .map(e -> e.replaceAll("’s", " "))
+                .map(e -> e.replaceAll("’", " "))
+                .map(e -> e.replaceAll("‘", " "))
+                .map(e -> e.replaceAll(":", " "))
+                .map(e -> e.replaceAll("#", " "))
+                .map(e -> e.replaceAll("/", " "))
+                .map(e -> e.replaceAll("-", " "))
+                .map(e -> e.replaceAll("\\.", " "))
+                .map(e -> e.replaceAll("\\*", " "))
+                .map(e -> e.replaceAll("\\(", " "))
+                .map(e -> e.replaceAll("\\)", " "))
+                .map(e -> e.replaceAll("\\[", " "))
+                .map(e -> e.replaceAll("\\]", " "))
                 .map(e -> e.split(" "))
+                .flatMap(Stream::of)
+                .filter(e -> !e.isEmpty())
                 .collect(Collectors.toList());
     }
 }
+
--- src/exercises/Week10MoreStream/ReflectionWeek10/AliceStatistics.java
+++ src/exercises/Week10MoreStream/ReflectionWeek10/AliceStatistics.java
@@ -1,38 +1,53 @@
 package exercises.Week10MoreStream.ReflectionWeek10;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class AliceStatistics {
 
     public static void main(String[] args) {
-        List<String> aliceInwonderland = (List<String>) new AliceReader();
-        getTheNumberOfWordsInAliceBook(aliceInwonderland);
-        getTheDistinictWordInAlicBook(aliceInwonderland);
-        getTheMostApearing5Words(aliceInwonderland);
-        getTheMostApearing5Letters(aliceInwonderland);
-        getTheNumerOfAliceApearingInAliceBook(aliceInwonderland);
+        List<String> words = new AliceReader().readAliceWords("exercises/Week10MoreStream/ReflectionWeek10/alice.txt");
+
+        getTheNumberOfWordsInAliceBook(words);
+        getTheNumberOfDistinctWords(words);
+        getLongestWord(words);
+        getTheMostApearing5Words(words);
+        getTheMostApearing5Letters(words);
+        getTheNumerOfAliceApearingInAliceBook(words);
 
     }
 
-    private static void getTheNumberOfWordsInAliceBook(List<String> aliceInwonderland) {
-        Long wordsNumber = aliceInwonderland.stream()
+
+    private static void getTheNumberOfWordsInAliceBook(List<String> words) {
+        Long wordsNumber = words.stream()
                 .collect(Collectors.counting());
-        System.out.println(wordsNumber);
+        System.out.println("Number of words:"+wordsNumber+"\n");
     }
 
-    private static void getTheDistinictWordInAlicBook(List<String> aliceInwonderland) {
-        List<String> distinctWords = aliceInwonderland.stream()
+    private static void getTheNumberOfDistinctWords(List<String> words) {
+        Long distinctWords = words.stream()
                 .distinct()
+                .collect(Collectors.counting());
+        System.out.println("Number of distinct words:"+distinctWords+"\n");
+    }
+    private static void getLongestWord(List<String> words) {
+        List<String> longestWord = words.stream()
+                .collect(Collectors.groupingBy(e -> Integer.valueOf(e.length()), Collectors.toList()))
+                .entrySet().stream()
+                .sorted((e1, e2) -> e2.getKey().compareTo(e1.getKey()))
+                .limit(1)
+                .map(Map.Entry::getValue)
+                .flatMap(List::stream)
                 .collect(Collectors.toList());
-        System.out.println(distinctWords);
+        System.out.println("Longest words:" +longestWord+"\n");
     }
 
 
 
-    private static void getTheMostApearing5Words(List<String> aliceInwonderland) {
-        List<String> mostAppearingWords = aliceInwonderland.stream()
+    private static void getTheMostApearing5Words(List<String> words) {
+        List<String> mostAppearingWords = words.stream()
                 .flatMap(e -> Stream.of(e))
                 .collect(Collectors.groupingBy(e -> e, Collectors.counting()))
                 .entrySet().stream()
@@ -40,13 +55,13 @@ public class AliceStatistics {
                 .limit(5)
                 .map(e -> e.getKey() + " | " + e.getValue())
                 .collect(Collectors.toList());
-        System.out.println(mostAppearingWords);
+        System.out.println("Most appearing 5 words:"+mostAppearingWords +"\n");
 
 
     }
 
-    private static void getTheMostApearing5Letters(List<String> aliceInwonderland) {
-        List<String> mostApearing5Letter = aliceInwonderland.stream()
+    private static void getTheMostApearing5Letters(List<String> words) {
+        List<String> mostApearing5Letter = words.stream()
                 .map(e -> e.split(""))
                 .flatMap(e -> Stream.of(e))
                 .collect(Collectors.groupingBy(e -> e, Collectors.counting()))
@@ -55,13 +70,13 @@ public class AliceStatistics {
                 .limit(5)
                 .map(e -> e.getKey() + " | " + e)
                 .collect(Collectors.toList());
-        System.out.println(mostApearing5Letter);
+        System.out.println("Most appearing 5 Letters:"+mostApearing5Letter+"\n");
 
 
     }
 
-    private static void getTheNumerOfAliceApearingInAliceBook(List<String> aliceInwonderland) {
-        Long alice = aliceInwonderland.stream()
+    private static void getTheNumerOfAliceApearingInAliceBook(List<String> words) {
+        Long alice = words.stream()
                 .filter(e -> e.equalsIgnoreCase("Alice"))
                 .collect(Collectors.counting());
         System.out.println("Alice appearances: "+alice);

a7e5b33cbd7d32fadc4439434c8458230f65bf9e
amin3696 <a11shahimian@gmail.com>
2019-01-29 14:21:05 +0100

    exercise01

---
+++ src/exercises/week11Time/exercise1/TimeTable.java
@@ -0,0 +1,19 @@
+package exercises.week11Time.exercise1;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeTable {
+
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.parse("10:00:00");
+        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("HH:mm");
+        String formatedTime = time.format(formatter);
+        System.out.println(formatedTime);
+
+        LocalDate date = LocalDate.parse("2003-03-13");
+        System.out.println(date.getDayOfWeek().toString());
+
+    }
+}
